cad95e6d6489aceb115986e68140ffbff7e4e521
Cookie <lucassrossi12@gmail.com>
2025-12-12 17:52:21 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vitejs.dev
 export default defineConfig({
   plugins: [react()],
+  base: "/cv-application-odin-project/",
 })
